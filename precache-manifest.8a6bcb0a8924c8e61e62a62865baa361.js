87045646544b12e65f6939e7803b447149d4514a
graciemzhang <g.m.zhang@wustl.edu>
2022-08-23 13:09:21 -0500

    Updates

---
+++ precache-manifest.8a6bcb0a8924c8e61e62a62865baa361.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97924467c590f85ecdea",
+    "url": "/react-fun/static/css/main.896aedd9.chunk.css"
+  },
+  {
+    "revision": "97924467c590f85ecdea",
+    "url": "/react-fun/static/js/main.97924467.chunk.js"
+  },
+  {
+    "revision": "fd0cfea648d4e488b62d",
+    "url": "/react-fun/static/js/1.fd0cfea6.chunk.js"
+  },
+  {
+    "revision": "8bac83854b47ea58c927",
+    "url": "/react-fun/static/js/runtime~main.8bac8385.js"
+  },
+  {
+    "revision": "28d94d988b745b1fc1f12e6d3193a902",
+    "url": "/react-fun/index.html"
+  }
+];
